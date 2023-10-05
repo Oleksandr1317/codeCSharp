e185fa0b494f88239057bcef1bb4a891749aa0d0
Oleksandr1317 <144147883+Oleksandr1317@users.noreply.github.com>
2023-10-05 16:50:19 +0300

    Add files via upload

---
+++ D_OOP/D_OOP/Interfaces.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_OOP
+{
+    public interface IBaseCollectrion
+    {
+        void Add(object obj);
+        void Remove(object obj);
+
+
+    }
+    public static class BaseCollectionExtencion
+    {
+        public static void AddRange(this IBaseCollectrion collection, IEnumerable<object> objects) // для создания метода-расширения
+        // IEnumerable - позволяет по инстанциям делать foreach (может быть предан как лист,так стек и т.д)
+        {
+            foreach (var item in objects)
+            {
+                collection.Add(item);
+            }
+        }
+    }
+
+    public class BaseList : IBaseCollectrion
+    {
+        private object[] items;
+        private int count = 0;
+        public BaseList(int InicialCapacity)
+        {
+            items = new object[InicialCapacity];
+        }
+        
+        public void Add(object obj)
+        {
+            items[count] = obj;
+            count++;
+        }
+
+        public void Remove(object obj)
+        {
+            items[count] = null;
+            count--;
+        }
+    }
+}
---
+++ D_OOP/D_OOP/ValRefTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_OOP
+{
+    public struct EvilStruct
+    {
+        public int X;
+        public int Y;
+
+        public PointRef PointRef;
+        
+    }
+    public struct PointVal
+    {
+        public int x; public int y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {x}. y = {y}");
+        }
+    }
+
+    public class PointRef
+    {
+        public int x; public int y;
+
+        public void LogValues()
+        {
+            Console.WriteLine($"x = {x}. y = {y}");
+        }
+    }
+}
