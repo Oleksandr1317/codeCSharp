4a7813461b0a2127e0e0a9f5ffaa8dad19286230
Oleksandr1317 <144147883+Oleksandr1317@users.noreply.github.com>
2023-10-05 16:53:36 +0300

    Add files via upload

---
+++ D_OOP/Homework_OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            double sq = square.CalcSquare(3.1, 3, 20);
+            double sq1 = square.CalcSquare(3.1, 3.2, 2.2);
+            Console.WriteLine(sq);
+            Console.WriteLine(sq1);
+        }
+    }
+}
