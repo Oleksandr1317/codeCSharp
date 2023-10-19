081cff9fdd5a5d9c45bf6fa72185273a795787df
Oleksandr1317 <144147883+Oleksandr1317@users.noreply.github.com>
2023-10-19 21:03:51 +0300

    Add files via upload

---
+++ Homework_3/gallows/Game.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gallows
+{
+    public class Game
+    {
+        private string randomWord;
+        private string hiddenWord;
+        private int count;
+        private HashSet<char> guessedLatters = new HashSet<char>();
+        bool isGameWin;
+        bool isGameLose;
+
+        public void StartGame()
+        {
+            string FilePath = "WordsStockRus.txt";
+            List<string> words = new List<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(FilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        words.Add(line);
+                    }
+                }
+                if (words.Count > 0)
+                {
+                    Random random = new Random();
+                    int indexrandom = random.Next(0,words.Count);
+                    randomWord = words[indexrandom];
+                    hiddenWord = new string('*', randomWord.Length);
+                    count = 6;
+                    isGameWin = false;
+                    isGameLose = false;
+                    Console.WriteLine($"Word : {hiddenWord}");
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("File reading error: " + ex.Message);
+            }
+        }
+        public void GuessLatter()
+        {
+            while(count > 0 && !isGameWin && !isGameLose)
+            {
+                Console.Write("Enter your latter: ");
+                char letter = char.Parse(Console.ReadLine().ToLower());
+                if (guessedLatters.Contains(letter))
+                {
+                    Console.WriteLine($"You have already entered a letter : '{letter}'");
+                    continue;
+                }
+
+                guessedLatters.Add(letter);
+
+                if (randomWord.Contains(letter))
+                {
+                    StringBuilder newhide = new StringBuilder(hiddenWord);
+                    for (int i = 0;i < randomWord.Length; i++)
+                    {
+                        if (Char.ToLower(randomWord[i]) == letter)
+                        {
+                            newhide[i] = letter;
+                        }
+
+                    }
+                    hiddenWord = newhide.ToString();
+                    Console.WriteLine($"Current word : {hiddenWord}");
+                }
+                else
+                {
+                    count--;
+                }
+
+                if(hiddenWord == randomWord)
+                {
+                    Console.WriteLine($"Congrats! You win.Word was {randomWord}");
+                    isGameWin = true;
+                }
+                else if (count == 0) { Console.WriteLine($"You lost!.Word was {randomWord}"); isGameLose = true; }
+            }
+        }
+    }
+}
---
+++ Homework_3/gallows/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gallows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.StartGame();
+            game.GuessLatter();
+        }
+    }
+}
